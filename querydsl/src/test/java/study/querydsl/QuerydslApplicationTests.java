1a50cc03fccb80535bba78cd22b98c75b1da5fc2
JHYUN <jhyun3315@naver.com>
2023-08-28 22:29:18 +0900

    h2 데이터베이스 연동 및 JPQL vs queryDSL 비교 TEST

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,44 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Transactional
 @SpringBootTest
+@Commit
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+
+
+		Hello result = query
+				.selectFrom(qHello) // Q 타입을 넣음
+				.fetchOne();
+
+		// 같은건지 확인
+		assertThat(result).isEqualTo(hello);
+		// 롬복 확인
+		assertThat(result.getId()).isEqualTo(hello.getId());
+
 	}
 
 }
