19772dc635c85122f12f90dba20312c938ce43c7
JHYUN <jhyun3315@naver.com>
2023-09-01 12:08:07 +0900

    조건 검색 AND 활용법

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,8 +2,8 @@ package study.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
-import org.junit.jupiter.api.BeforeEach;import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +13,8 @@ import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static study.querydsl.entity.QMember.*;
@@ -77,14 +79,64 @@ public class QuerydslBasicTest {
 
     }
 
+    // 이름이 "member1"이면서 age가 10~30 사이인 사람 조회
     @Test
     public void search(){
-        Member findMember = queryFactory
+        Member findMember  = queryFactory
                 .selectFrom(member)
-                .where(member.username.eq("member1").and(member.age.eq(10)))
-                .fetchOne();
+                .where(member.username.eq("member1")
+                .and(member.age.between(10, 30)))
+                .fetchOne();  // 단건 조회
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
+
+    // 이름이 "member1"이면서 age가 10~30 사이인 사람 조회
+    @Test
+    public void searchAndParam(){
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.eq("member1"),
+                        member.age.between(10, 30), // ','로 chain을 사용하여 and 조건 구분
+                        null // null 은 무시됨
+                )
+                .fetchOne();  // 단건 조회
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void searchExample(){
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(
+                        member.username.ne("member1"), // member1이 아니고( .eq("member1").not() 와 동일 ),
+                        member.age.goe(30) // age >= 30 인 경우
+
+                        /*
+                            member.username.eq("member1") // username = 'member1'
+                            member.username.ne("member1") //username != 'member1'
+                            member.username.eq("member1").not() // username != 'member1'
+                            member.username.isNotNull() //이름이 is not null
+                            member.age.in(10, 20) // age in (10,20)
+                            member.age.notIn(10, 20) // age not in (10, 20)
+                            member.age.between(10,30) //between 10, 30
+                            member.age.goe(30) // age >= 30
+                            member.age.gt(30) // age > 30
+                            member.age.loe(30) // age <= 30
+                            member.age.lt(30) // age < 30
+                            member.username.like("member%") //like 검색
+                            member.username.contains("member") // like ‘%member%’ 검색
+                            member.username.startsWith("member") //like ‘member%’ 검색
+
+                     */
+                )
+                .fetchOne();  // 단건 조회
+
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+    
 
 }
